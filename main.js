17369a0f782f073d7b81bbd7d436ad9022cd9caf
Alma <alma9000@gmail.com>
2019-10-20 14:04:47 +0200

    My first website commit
    I followed a guide called "publish and share  your own website for free with GitHub" by Scott Vinkle

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+
+/* MAKE KYLLING WAG ON HOVER */
+$(function()
+{
+    $("#mylogo").hover(
+        function() {
+            $(this).attr("src", "src/Kyllogo.gif");
+        },
+        function() {
+            $(this).attr("src", "src/Kyllogo.png");
+        }
+    );
+});
+
+
+function changeImg(){
+var myimg = document.getElementById("randombanner");
+var imgarray = ["src/ads/buysausage.gif", "src/ads/hotdogsad.gif", "src/ads/dogfood.jpeg"];
+var imgindex = Math.floor(Math.random() * imgarray.length);
+console.log(imgindex);
+
+    myimg.setAttribute("src", imgarray[imgindex]);
+
+}
+
+changeImg();
+                    

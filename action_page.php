ecc933cfbd4486d4a58ef4681affb88b61170ed2
Alma <alma9000@gmail.com>
2019-10-31 21:52:20 +0100

    bux fixing

--- action_page.php
+++ action_page.php
@@ -2,17 +2,18 @@
 
 if(isset($_POST['submit']))
 {
+    /* declaring variables*/
     $name = $_POST['name'];
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
-    $subject = "Message from Kylling Spree"
+    $subject = $_POST['subject'];
     
     
     $mailTo = "almatem@roskilde.dk";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
-    
+    /*using variables in already existing functions*/
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 }

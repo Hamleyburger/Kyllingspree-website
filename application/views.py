28d7bcc4af61d75dc28dc070e4bddbf7bcdc8b39
Alma <alma9000@gmail.com>
2020-02-22 19:59:10 +0100

    Added comments and added e-mail address to message
    - More comments to make clear what does what in sendMail function
    - The recepient will now be able to see what e-mail address the user has
      provided. Doesn't mean it'll exist though.

--- application/views.py
+++ application/views.py
@@ -31,7 +31,7 @@ def contact():
         message = req.get("message")
 
         print(req)
-        sendMail(message, subject)
+        sendMail(message, subject, email)
 
         return render_template("submitted.html", email=email, subject=subject, message=message)
 
@@ -39,11 +39,17 @@ def contact():
 
 
 
-def sendMail(usermessage, usersubject):
+#Sends e-mail with a message stating a message and the user provided e-mail address
+def sendMail(usermessage, usersubject, useremail):
 
+    #The e-mail for which the password must match
     sender_email = cfg.mySecrets['sender_email']
+    #The recipient e-mail. Can be anything.
     rec_email = cfg.mySecrets['rec_email']
+    #Password for sender e-mail. Can be a headache with two factor authorization.
     password = cfg.mySecrets['password']
+    #Message containing user provided message and e-mail address
+    message = "From: {}\n\n{}".format(useremail, usermessage)
 
 
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
@@ -52,8 +58,7 @@ def sendMail(usermessage, usersubject):
         smtp.ehlo()
         smtp.login(sender_email, password)
      
-
-        msg = f'Subject: {usersubject}\n\n{usermessage}'
+        msg = f'Subject: {usersubject}\n\n{message}'
 
         smtp.sendmail(sender_email, rec_email, msg)
 

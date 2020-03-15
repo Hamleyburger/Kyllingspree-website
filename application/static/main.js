cbb92c8108f4d7e1d080a1bad658fdf297928bc7
Alma <alma9000@gmail.com>
2020-03-15 23:56:41 +0100

    Tomato now fully functional

--- application/static/main.js
+++ application/static/main.js
@@ -2,33 +2,58 @@
 
 
 /* MAKE KYLLING WAG ON HOVER */
-$(function()
-{
+$(function () {
     $("#mylogo").hover(
-        function() {
+        function () {
             $(this).attr("src", "/static/src/navimgs/Kyllogo.gif");
         },
-        function() {
+        function () {
             $(this).attr("src", "/static/src/navimgs/Kyllogo.png");
         }
     );
 });
 
 
-function changeImg(){
-var myimg = document.getElementById("randombanner");
-var imgarray = ["/static/src/ads/hotdogsad.gif", "/static/src/ads/buysausage.gif", "/static/src/ads/dogfood.jpeg", "/static/src/ads/nonormalgame.png", "/static/src/ads/sharpteeth.png"];
-var imgindex = Math.floor(Math.random() * imgarray.length);
-console.log(imgindex);
+function setColourClass() {
+    document.body.classList.add(sessionStorage.getItem("colourPref"));
+}
+$(document).ready(setColourClass);
+
+function changeImg() {
+    var myimg = document.getElementById("randombanner");
+    var imgarray = ["/static/src/ads/hotdogsad.gif", "/static/src/ads/buysausage.gif", "/static/src/ads/dogfood.jpeg", "/static/src/ads/nonormalgame.png", "/static/src/ads/sharpteeth.png"];
+    var imgindex = Math.floor(Math.random() * imgarray.length);
+    console.log(imgindex);
 
     myimg.setAttribute("src", imgarray[imgindex]);
 
 }
 
 function formAlert() {
-  alert("Apologies! Kylling is currently closed to new requests.");
-  return false
+    alert("Apologies! Kylling is currently closed to new requests.");
+    return false
 }
 
 changeImg();
+
+
+function changeColour(colourClass) {
+
+    var bclass = document.body.classList;
+    var key = "colourPref";
+
+    if (bclass.contains(colourClass)) {
+        bclass.remove(colourClass)
+        sessionStorage.setItem(key, "noColour");
+        bclass.add(sessionStorage.getItem(key));
+    }
+    else {
+        bclass.remove(sessionStorage.getItem(key));
+        bclass.add(colourClass);
+        sessionStorage.setItem(key, colourClass);
+    }
+}
+
+
+
 
